7701972b37cc1456972943e3c2531f69bbb9b881
Guilherme Marques <gpmrks@GPMrks>
2023-02-17 15:14:17 -0300

    FEAT - Improved ExceptionHandler to return ProblemDetail instead of ResponseEntity according to new Spring features

--- src/main/java/com/attornatus/backenddevelopertest/controller/EnderecoControllerAdvice.java
+++ src/main/java/com/attornatus/backenddevelopertest/controller/EnderecoControllerAdvice.java
@@ -2,11 +2,13 @@ package com.attornatus.backenddevelopertest.controller;
 
 import com.attornatus.backenddevelopertest.exception.EnderecoNaoEncontradoException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,10 +16,13 @@ import java.util.Map;
 public class EnderecoControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
-    public final ResponseEntity<Object> enderecoNaoEncontrado(EnderecoNaoEncontradoException ex) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("message", ex.getMessage());
-        body.put("status code", 404);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+    public ProblemDetail enderecoNaoEncontrado(EnderecoNaoEncontradoException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getLocalizedMessage());
+        problemDetail.setTitle("Endereço não encontrado com este ID");
+        problemDetail.setDetail("É preciso informar um ID de um endereço existente");
+        problemDetail.setProperty("Categoria", "Endereço");
+        problemDetail.setProperty("TimeStamp", Instant.now());
+        problemDetail.setProperty("Stacktrace", ex.getStackTrace());
+        return problemDetail;
     }
 }
--- src/main/java/com/attornatus/backenddevelopertest/controller/PessoaControllerAdvice.java
+++ src/main/java/com/attornatus/backenddevelopertest/controller/PessoaControllerAdvice.java
@@ -2,11 +2,13 @@ package com.attornatus.backenddevelopertest.controller;
 
 import com.attornatus.backenddevelopertest.exception.PessoaNaoEncontradaException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.net.URI;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,10 +16,13 @@ import java.util.Map;
 public class PessoaControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
-    public final ResponseEntity<Object> pessoaNaoEncontrada(PessoaNaoEncontradaException ex) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("message", ex.getMessage());
-        body.put("status code", 404);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+    public ProblemDetail pessoaNaoEncontrada(PessoaNaoEncontradaException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getLocalizedMessage());
+        problemDetail.setTitle("Pessoa não encontrada com este ID");
+        problemDetail.setDetail("É preciso informar um ID de uma pessoa existente");
+        problemDetail.setProperty("Categoria", "Pessoa");
+        problemDetail.setProperty("TimeStamp", Instant.now());
+        problemDetail.setProperty("Stacktrace", ex.getStackTrace());
+        return problemDetail;
     }
 }
